bfc89b3d2150271151ac72bab59d7e58c82b8618
Kiran Thirumalaichery Dhakshinamoorthy <kirandhakshina@gmail.com>
2022-05-21 13:53:59 -0500

    add store

---
+++ src/features/counter/counter-slice.ts
@@ -0,0 +1,30 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface CounterState {
+    value: number;
+}
+
+const initialState: CounterState = {
+    value: 0
+};
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.value++;
+        },
+        decrement(state) {
+            state.value--;
+        },
+        reset(state) {
+            state.value = 0;
+        }
+    }
+});
+
+export const { increment } = counterSlice.actions;
+
+export default counterSlice.reducer;
+

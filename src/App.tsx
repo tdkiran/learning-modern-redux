7d7ce5066f40bfe3a277949ae220e3a5d15f0901
Kiran Thirumalaichery Dhakshinamoorthy <kirandhakshina@gmail.com>
2022-05-21 13:54:07 -0500

    connect store to comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,25 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { increment } from "./features/counter/counter-slice";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <p>Counter</p>
+        <button type="button" onClick={() => dispatch(increment)}>
+          Add
+        </button>
+        count is: {count}
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
